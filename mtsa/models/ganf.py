07a234e01371339cfac4135cd949590c726f4cd1
cristoferSilva <gabrielc2828@gmail.com>
2024-04-04 18:29:59 -0300

    Adjust Components

--- mtsa/models/ganf.py
+++ mtsa/models/ganf.py
@@ -13,65 +13,12 @@ from functools import reduce
 from torch.nn.init import xavier_uniform_
 from torch.nn.utils import clip_grad_value_
 from torch.utils.data import TensorDataset, DataLoader
-from torch.utils.data import Dataset
+from mtsa.models.GANF_model_components.audiodata import AudioData
+from mtsa.models.GANF_model_components.gnn import GNN
 from mtsa.utils import Wav2Array
 from sklearn.pipeline import Pipeline
 import pandas as pd
-
 from mtsa.utils import Wav2Array
-
-class AudioData(Dataset):
-    def __init__(self, df, window_size=12, stride_size=1):
-        super(AudioData, self).__init__()
-        self.df = df
-        self.window_size = window_size
-        self.stride_size = stride_size
-
-        self.data, self.idx, self.time = self.preprocess(df)
-    
-    def preprocess(self, df):
-        start_idx = np.arange(0,len(df)-self.window_size,self.stride_size)
-        end_idx = np.arange(self.window_size, len(df), self.stride_size)
-
-        delat_time =  df.index[end_idx]-df.index[start_idx]
-        idx_mask = delat_time==pd.Timedelta(self.window_size,unit='s')
-
-        return df.values, start_idx[idx_mask], df.index[start_idx[idx_mask]]
-
-    def __len__(self):
-
-        length = len(self.idx)
-
-        return length
-
-    def __getitem__(self, index):
-        #  N X K X L X D 
-        start = self.idx[index]
-        end = start + self.window_size
-        data = self.data[start:end].reshape([self.window_size,-1, 1])
-
-        return torch.FloatTensor(data).transpose(0,1)
-
-class GNN(nn.Module):
-    """
-    The GNN module applied in GANF
-    """
-    def __init__(self, input_size, hidden_size):
-        super(GNN, self).__init__()
-        self.lin_n = nn.Linear(input_size, hidden_size)
-        self.lin_r = nn.Linear(input_size, hidden_size, bias=False)
-        self.lin_2 = nn.Linear(hidden_size, hidden_size)
-
-    def forward(self, h, adjacent_matrix):
-        ## adjacent_matrix: K X K
-        ## H: N X K  X L X D
-
-        h_n = self.lin_n(torch.einsum('nkld,kj->njld',h,adjacent_matrix))
-        h_r = self.lin_r(h[:,:,:-1])
-        h_n[:,:,1:] += h_r
-        h = self.lin_2(F.relu(h_n))
-
-        return h
 
 class GANFBaseModel(nn.Module):
     def __init__ (self, 

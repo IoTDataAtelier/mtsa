6bc158447aa35f89a86ec08cc4b85d1f7a4a7394
jonLucas33 <joaolucaslopes296@gmail.com>
2024-08-19 22:15:04 -0300

    Organization of environment and start of implementation.

---
+++ mtsa/models/transformer_components/input_embedding.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+import math
+
+class InputEmbedding(nn.Module):
+    
+    def __init__(self, d_model: int, vocab_size: int):
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, d_model)
+        
+    def forward(self, x):
+        return self.embedding(x) * math.sqrt(self.d_model)

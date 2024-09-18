12aee531ae31fa7095316c862b5732f47218e4b8
Joao Lucas <joaolucaslopes296@gmail.com>
2024-09-18 04:06:33 -0300

    Added sound.wav and notebooks.

---
+++ mtsa/models/transformer_model2.py/layer_normalization.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+import math
+
+class LayerNormalization(nn.Module):
+    def __init__(self, eps: float = 10**-6) -> None:
+        super().__init__()
+        self.eps = eps 
+        self.alpha = nn.Parameter(torch.ones(1))
+        self.bias = nn.Parameter(torch.zeros(1))
+        
+    def forward(self, x):
+        mean = x.mean(dim = -1, keepdim=True)
+        std = x.std(dim = -1, keepdim=True)
+        return self.alpha * (x - mean) / (std + self.eps) + self.bias
+        
+    
+class ResidualConnection(nn.Module):
+    
+        def __init__(self, features: int, dropout: float) -> None:
+            super().__init__()
+            self.dropout = nn.Dropout(dropout)
+            self.norm = LayerNormalization(features)
+    
+        def forward(self, x, sublayer):
+            return x + self.dropout(sublayer(self.norm(x)))
